911dc498221c1027b7542aa7c0e0820cdb908955
Jacquelyn Moreno <jemoreno802@gmail.com>
2018-03-06 15:45:08 -0500

    added 2 extra credit problems

---
+++ LiarsKB.java
@@ -0,0 +1,69 @@
+
+public class LiarsKB extends KB{
+	Prover prove = new Prover();
+	public LiarsKB(int i) {
+		super();
+		Symbol A = intern("A");
+		Symbol B = intern("B");
+		Symbol C = intern("C");
+		
+		if(i == 1) {
+			add(new Implication(A, new Conjunction(A, C))); 					//A->A ^ C
+			add(new Implication(B, new Negation(C)));						//B->NOT(C)
+			add(new Implication(C, new Disjunction(B, new Negation(A))));		//C->B V NOT(A)
+		}if(i==2) {
+			add(new Implication(A, new Negation(C)));
+			add(new Implication(B, new Conjunction(A,C)));
+			add(new Implication(C,B));
+		}if(i == 3) {
+			Symbol D = intern("D");
+			Symbol E = intern("E");
+			Symbol F = intern("F");
+			Symbol G = intern("G");
+			Symbol H = intern("H");
+			Symbol I = intern("I");
+			Symbol J = intern("J");
+			Symbol K = intern("K");
+			Symbol L = intern("L");
+			
+			add(new Implication(A, new Conjunction(H, I)));
+			add(new Implication(B, new Conjunction(A, L)));
+			add(new Implication(C, new Conjunction(B, G)));
+			add(new Implication(D, new Conjunction(E, L)));
+			add(new Implication(E, new Conjunction(C, H)));
+			add(new Implication(F, new Conjunction(D, I)));
+			add(new Implication(G, new Conjunction(new Negation(E), new Negation(J))));
+			add(new Implication(H, new Conjunction(new Negation(F), new Negation(K))));
+			add(new Implication(I, new Conjunction(new Negation(G), new Negation(K))));
+			add(new Implication(J, new Conjunction(new Negation(A), new Negation(C))));
+			add(new Implication(K, new Conjunction(new Negation(D), new Negation(F))));
+			add(new Implication(L, new Conjunction(new Negation(B), new Negation(J))));
+		}
+		
+	}
+	
+	public void checkModels(int i) {
+		if(i == 1||i==2) {
+			System.out.println("KB entails(Amy is a truth teller): " + prove.entails(this, intern("A")));
+			System.out.println("KB entails(Bob is a truth teller): " + prove.entails(this, intern("B")));
+			System.out.println("KB entails(Cal is a truth teller): " + prove.entails(this, intern("C")));
+		}if(i==3) {
+			System.out.println("KB entails(Amy is a truth teller): " + prove.entails(this, intern("A")));
+			System.out.println("KB entails(Bob is a truth teller): " + prove.entails(this, intern("B")));
+			System.out.println("KB entails(Cal is a truth teller): " + prove.entails(this, intern("C")));
+			System.out.println("KB entails(Dee is a truth teller): " + prove.entails(this, intern("D")));
+			System.out.println("KB entails(Eli is a truth teller): " + prove.entails(this, intern("E")));
+			System.out.println("KB entails(Fay is a truth teller): " + prove.entails(this, intern("F")));
+			System.out.println("KB entails(Gil is a truth teller): " + prove.entails(this, intern("G")));
+			System.out.println("KB entails(Hal is a truth teller): " + prove.entails(this, intern("H")));
+			System.out.println("KB entails(Ida is a truth teller): " + prove.entails(this, intern("I")));
+			System.out.println("KB entails(Jay is a truth teller): " + prove.entails(this, intern("J")));
+			System.out.println("KB entails(Kay is a truth teller): " + prove.entails(this, intern("K")));
+			System.out.println("KB entails(Lee is a truth teller): " + prove.entails(this, intern("L")));
+		}
+		
+
+		
+	}
+
+}
--- main.java
+++ main.java
@@ -13,7 +13,9 @@ public class main {
 			System.out.println("Enter 1 to test Modus Ponens");
 			System.out.println("Enter 2 to test Wumpus World");
 			System.out.println("Enter 3 to test Horn Clauses");
-			System.out.println("Enter 4 to test Part 2 (Resolution)");
+			System.out.println("Enter 4 to test Liars and Truth Tellers");
+			System.out.println("Enter 5 to test More Liars and Truth Tellers");
+			System.out.println("Enter 6 to test Part 2 (Resolution)");
 			answer = scan.nextInt();
 			
 			if(answer == 1) {
@@ -22,7 +24,15 @@ public class main {
 				new WumpusWorldKB().checkModels();
 			}if(answer == 3) {
 				new HornClausesKB().checkModels();
-			}if(answer == 4) {
+			}if(answer==4) {
+				System.out.println("Part a: ");
+				new LiarsKB(1).checkModels(1);
+				System.out.println("Part b: ");
+				new LiarsKB(2).checkModels(2);
+			}if(answer == 5) {
+				new LiarsKB(3).checkModels(3);
+			}
+			if(answer == 6) {
 				System.out.println("Testing resolution");
 				System.out.println("Enter 0 to exit");
 				System.out.println("Enter 1 to test Modus Ponens");
